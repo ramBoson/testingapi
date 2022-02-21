c217b29508eb0bf02e90f7e887c07446e16b21b5
ramBoson <2020ramboson@gmail.com>
2022-02-21 10:01:00 +0530

    done vercel also worked

--- src/App.js
+++ src/App.js
@@ -397,121 +397,6 @@ import url from './configurl';
 
 function App() {
 
-
- const postValues =async()=>{
-   
-  const requestOptions = {
-    method: 'POST',
-    headers:{
-          'Authorization': `Bearer vATR2p2l8cv0BVbpEQ2u9KzdXGW5nMgZ`,
-          "Access-Control-Allow-Headers" : "*",
-          "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PUT",
-          "Access-Control-Allow-Credentials":"true"
-    },
-    body: JSON.stringify({ 
-            client_id:"cEZoGE19mLmQdIPPjXtj2osurm8NRLHK",
-            client_secret:"VNe8u0lpgcCvE9NsE7Khcft7gA22RMvW",
-            grant_type:"client_credentials",
-            scope:"email"		 
-    })
-  };
-  // const requestOptions = {
-  //   method:'POST',    
-  //   param: { 
-       
-  //           client_id:"cEZoGE19mLmQdIPPjXtj2osurm8NRLHK",
-  //           client_secret:"VNe8u0lpgcCvE9NsE7Khcft7gA22RMvW",
-  //           grant_type:"client_credentials",
-  //           scope:"email"		 
-  //   },
-  //   headers:{
-  //         'Authorization': `Bearer vATR2p2l8cv0BVbpEQ2u9KzdXGW5nMgZ`,
-  //         "Access-Control-Allow-Headers" : "*",
-  //         "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PUT",
-  //         "Access-Control-Allow-Credentials":"true"
-  //   }
-  // }
-// const response = await axios.post("https://api.elementpad.io/elementsapi/oauth2/token?client_id=cEZoGE19mLmQdIPPjXtj2osurm8NRLHK&client_secret=VNe8u0lpgcCvE9NsE7Khcft7gA22RMvW&grant_type=client_credentials&scope=email");
-
-const response = await axios.post("https://api.elementpad.io/elementsapi/oauth2/token", requestOptions);
-// const data = await response.json();  
-console.log("dataresponse",response) ;  
- }
-
-
- const newaxiosmethod=async()=>{   
-  const requestOptions = {
-    method: 'POST',    
-      "headers": [
-        {
-          "source": "/api/(.*)",
-          "headers": [
-            { "key": "Access-Control-Allow-Credentials", "value": "true" },
-            { "key": "Access-Control-Allow-Origin", "value": "*" },
-            { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-          ]
-        }
-      ],
-    
-    // headers:{
-    //       'Authorization': `Bearer 6e5Zf60srxBtXYjz5F1IoPwC40AJcg3d`,
-    //       "Access-Control-Allow-Headers" : "*",
-    //       "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PUT",
-    //       "Access-Control-Allow-Credentials":"true"
-    // },
-    body: JSON.stringify({ 
-            client_id:"cEZoGE19mLmQdIPPjXtj2osurm8NRLHK",
-            client_secret:"VNe8u0lpgcCvE9NsE7Khcft7gA22RMvW",
-            grant_type:"client_credentials",
-            scope:"email"		 
-    })
-  };
-  console.log("Before Axios")
-  await axios.post("https://api.elementpad.io/elementsapi/oauth2/token",requestOptions)
- .then(async(responseuser) => {
-   console.log("uploadeduser",responseuser)                                         
- })
- .catch((e) => {
-   console.log("error",e);     
- })                                                  
-
- }
-
-
- 
- //res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
- //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- 
-
- const getvalues = async()=>{
-  //4ARS96PN27JeY7NyubNjArv9jc4FOL4o
-  //vATR2p2l8cv0BVbpEQ2u9KzdXGW5nMgZ
-  let access_token = "XsQzv4q16GWMv9bolPuYPPdQOUMncoUa";
-  let s =  await axios.request(`https://api.elementpad.io/elementsapi/v1/users`,{headers:{
-        'Authorization': `Bearer XsQzv4q16GWMv9bolPuYPPdQOUMncoUa`,
-        "Access-Control-Allow-Headers" : "*",
-        "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PUT",
-        "Access-Control-Allow-Credentials":"true",
-        "accept": "application/json, text/plain, */*",
-        "authorization": "REDACTED",
-  }}
-  )
-
-  
-  
-//   let s = await fetch("https://3.139.93.222:42102/elementsapi/v1/users/",{headers:{
-//     'Authorization': "Bearer 4Ro0FgB38F68WXzIdusdCCMLFwu4t9cu",
-//     "Access-Control-Allow-Headers" : "Content-Type",
-//             "Access-Control-Allow-Origin": "https://api.elementpad.io/",
-//             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-// }},{mode:'no-cors'})
-// let s = await axios.post("https://api.elementpad.io/elementsapi/v1/users/?Authorization=Bearer bOxj41yiRCTLoW7cMCsKa8DoQ9Ufhu2w")
-  console.log("get",s)
-  
-    
-}
-
 const getuser = async()=>{
 
 const options = {
@@ -555,60 +440,14 @@ const getanotherquerry = async() =>{
 }
 
 
-const extraget=()=>{
-  const options = {
-    method: 'GET',
-    url: 'https://7oigah6lib.execute-api.us-east-2.amazonaws.com/DEV',
-    //https://7oigah6lib.execute-api.us-east-2.amazonaws.com/DEV
-    // headers: {
-    //   Accept: 'application/json',
-    //   'Content-Type': 'application/json',
-    //   Authorization: 'Bearer vhQxkXyA0X0oE1Rm20A03J7Azhp6skkH'
-    // }
-  };
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
-  
-}
-
   return (
-    <div className="App">
-                   
-                    <button onClick={()=>postValues()}>
-                       POST
-                    </button>
-<br></br>
-                    <button onClick={()=>getvalues()}>
-                       GET
-                    </button>
-
-
-                   
-<br></br>
+    <div className="App">                                   
                     <button onClick={()=>getanotherquerry()}>
                        Sample
                     </button>
-
                     <button onClick={()=>getuser()}>
                        Sample-2 Get
-                    </button>
-
-                    <br></br>
-                    <button onClick={()=>newaxiosmethod()}>
-                       New POST
-                    </button>
-                    <br></br>
-                    <button onClick={()=>extraget()}>
-                       Extra
-                    </button>
-
-
-
-
-                    
+                    </button>                                    
     </div>
   );
 }
